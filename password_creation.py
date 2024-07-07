74b6dfe3db147f4ab710f3c52820d6008fc28250
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 15:47:55 +0530

    Add files via upload
    password creation tool that generates strong and secure passwords based on user-specified criteria, enhancing cybersecurity by promoting robust password practices.

---
+++ password_creation.py
@@ -0,0 +1,35 @@
+# import random
+# letters=['a','b','c','d','e','f','h','i','j','k','l','m','n','o','p',
+#          'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E',
+#          'F','I','J','K','L','M','N','O','P','Q','R','S','T','U','V',
+#          'W','X','Y','Z']
+# numbers=['0','1','2','3','4','5','6','7','8','9']
+
+# symbols=['!','@','#','$','%','&','*','+']
+
+# print('Welcome To Password Generator')
+
+# n_letters=int(input('how many letters you want in your password?\n'))
+# n_numbers=int(input('how many numbers you want in your password?\n'))
+# n_symbols=int(input('how many symbols you want in your password?\n'))
+
+# password=[]
+# for i in range(1,5):
+#     char=random.choice(letters)
+#     password+=char
+
+# for i in range(1,4):
+#     char=random.choice(numbers)
+#     password+=char
+
+# for i in range(1,3):
+#     char=random.choice(symbols)
+#     password+=char
+
+# print(password)
+# random.shuffle(password)
+# print(password)
+# my_string="".join(password)
+# print(my_string)
+
+    
