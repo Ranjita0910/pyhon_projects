c9775bde9b5159ccb6d5d58b9d2c724a7ef5632d
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 15:44:04 +0530

    Add files via upload
    project it enables users to check balances, withdraw funds, deposit money, and manage transactions securely, replicating real-world ATM functionalities.

---
+++ atm_working.py
@@ -0,0 +1,44 @@
+# import time
+# print("please insert your card")
+
+# time.sleep(5)
+
+# password=1926
+# balance=10000
+
+# pin=int(input("enter your atm pin"))
+# if pin==password:
+#     while True:
+#         print('''
+#           1==balance
+#           2==withdraw balance
+#           3==deposit balance
+#           4==exit
+#           '''
+#           )
+#         try:
+#             option=int(input("please enter your choice"))
+#         except:
+#             print("please enter valid option")
+#         if option==1:
+#             print(f"your current balance is {balance}")
+#         if option==2:
+#             withdraw_amount=int(input("please enter withdraw_amount"))
+#             balance=balance=withdraw_amount
+
+#             print(f"{withdraw_amount} is debited from your account")
+#             print(f"your current balance if {balance}")
+#         if option==3:
+#             deposit_amount=int(input("please enter deposit_amount"))
+#             balance=balance=withdraw_amount
+
+#             print(f"{deposit_amount} is credited to your account")
+#             print(f"your updated balance if {balance}")
+    
+#         if option==4:
+#             break
+
+    
+        
+# else:
+#     print("wrong pin please try again")
