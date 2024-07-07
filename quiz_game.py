f1d73ded58fcd78a291dd255875944a16b4c5e45
Ranjita0910 <ranjitamelmuri200@gmail.com>
2024-07-07 15:33:40 +0530

    Add files via upload
    This project includes the initial version of the quiz _game script, which contains basic  functionality for asking questions and checking answers.

---
+++ quiz_game.py
@@ -0,0 +1,53 @@
+# print("welcome to my Karnataka quiz!")
+
+# playing= input("do you want to play? ")
+# if playing!="yes":
+#     quit()
+
+# print("okay! Let's play :)")
+# score=0
+
+# answer=int(input("In which year was Mysore renamed Karnataka? "))
+# if answer ==1973:
+#     print("correct!")
+#     score+=1
+# else:
+#     print("incorrect!")
+
+
+# answer=input("Which is the State flower of Karnataka? ")
+# if answer=="Lotus":
+#     print("correct!")
+#     score+=1
+# else:
+#     print("incorrect!")
+
+
+# answer=input("At which place is the Suvarna Vidhana Soudha located? ")
+# if answer =="Belagavi":
+#     print("correct!")
+#     score+=1
+# else:
+#     print("incorrect!")
+
+
+# answer=input("At which place is the historical monument Gol Gumbaz located? ")
+# if answer =="Vijayapura":
+#     print("correct!")
+#     score+=1
+# else:
+#     print("incorrect!")
+
+
+# answer=int(input("In which year was Kannada granted the status of a classical language by the Central Govt? "))
+# if answer ==2008:
+#     print("correct!")
+#     score+=1
+# else:
+#     print("incorrect!")
+
+# print("you got" +str(score)+"questions correct!")
+# print("you got" +str((score/5)*100) +"%.")
+
+    
+
